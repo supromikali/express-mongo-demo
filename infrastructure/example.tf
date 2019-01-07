e209df73b43f46767049d792db67b033b28cfd1c
Alexander Solovyev <supromikali@yandex.ru>
2019-01-07 08:18:58 -0800

    Infrastructure: add vpc, subnet and security groups #9

--- infrastructure/example.tf
+++ infrastructure/example.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,9 +26,25 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+module "security_group" {
+  source      = "terraform-aws-modules/security-group/aws"
+  version     = "2.7.0"
+
+  name        = "example"
+  description = "Security group"
+  vpc_id      = "${data.aws_vpc.default.id}"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+}
+
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+
+  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
+  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
 
   tags {
     Name = "HelloWorld"
