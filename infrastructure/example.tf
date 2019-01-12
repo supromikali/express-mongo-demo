7069b6479193caa6366188a18f499df94ffca1e4
Alexander Solovyev <supromikali@yandex.ru>
2019-01-12 12:18:42 +0300

    Infrastructure: fix feedback issues #9

--- infrastructure/example.tf
+++ infrastructure/example.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
-  vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1c"
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1c"
 
   tags = {
     Name = "main subnet"
@@ -32,40 +32,52 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_security_group" "main" {
-	name = "main"
-	description = "Allow SSH TCP Security Group"
-
-	ingress {
-		from_port = 80
-		to_port = 80
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
+resource "aws_security_group_rule" "app_server_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.allow-ssh-http-ping.id}"
 }
 
-resource "aws_instance" "web" {
+resource "aws_security_group_rule" "app_server_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.allow-ssh-http-ping.id}"
+}
+
+resource "aws_security_group_rule" "app_server_ping" {
+  type              = "ingress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.allow-ssh-http-ping.id}"
+}
+
+resource "aws_security_group" "allow-ssh-http-ping" {
+  name        = "allow-ssh-http-ping"
+  description = "Allow SSH TCP Ping Security Group"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "express-mongo-demo" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
-  subnet_id                   = "${aws_subnet.main.id}"
-  vpc_security_group_ids      = ["${aws_security_group.main.name}"]
+  subnet_id              = "${aws_subnet.public_subnet.id}"
+  vpc_security_group_ids = ["${aws_security_group.allow-ssh-http-ping.name}"]
 
   tags {
-    Name = "web"
+    Name = "express-mongo-demo"
   }
 }
